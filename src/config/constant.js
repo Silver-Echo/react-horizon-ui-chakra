9125a97e8ce251aaae6f00b271c93b3b2afc67b5
Silver-Echo <144827297+Silver-Echo@users.noreply.github.com>
2023-11-17 04:07:50 +0000

    Pending changes exported from your codespace

--- src/config/constant.js
+++ src/config/constant.js
@@ -4,5 +4,8 @@ if (process.env.REACT_APP_BACKEND_SERVER) {
 } else {
   BACKEND_SERVER = "http://localhost:5000/api/";
 }
-
+const config = {
+  ...
+  API_SERVER: 'http://localhost:5000/api/'  // <-- The magic line
+};
 export const API_SERVER = BACKEND_SERVER;
